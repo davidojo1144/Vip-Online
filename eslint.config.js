ba76eb4295c14a277d1c162dee1fa8440b87212d
davidojo1144 <davco1144@gmail.com>
2026-03-13 10:45:45 +0100

    chore(lint): add flat ESLint config via eslint-config-expo

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    rules: {
+      // keep Prettier only as formatter, not as linter
+    },
+  },
+]);
